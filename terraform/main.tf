02c8e60c455f927563bf6e5e545f8d4a95b8ccfa
munemo <john.chimbani@outlook.com>
2022-11-22 11:12:57 +0100

    removing excess files

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,11 +20,10 @@ module "azurerm_app_service_plan" {
   app_service_name    = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tier = "Dynamic"
+  size = "Y1"
 
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
-  }
+
 
 }
 

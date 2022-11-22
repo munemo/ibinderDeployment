7bbc4e08c41f95dd0b5728e1f9b51b9a8ff64298
munemo <john.chimbani@outlook.com>
2022-11-22 12:56:56 +0100

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,13 @@
+provider "azurerm" {
+   features {}
+}
+
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
 }
+
 
 
 resource "random_string" "random" {

4d588a230c5fcfeaff79712821784e8ba4a5034a
munemo <john.chimbani@outlook.com>
2022-11-22 11:02:19 +0100

    removing excess files

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,12 @@ module "app_service_plan" {
   app_service_name    = var.app_service_name
   location            = var.location
   resource_group_name = var.resource_group_name
+
+sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+
 }
 
 module "azure_function_app" {

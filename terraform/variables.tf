15686491d7d95056dda001a1e4df01b252d71573
munemo <john.chimbani@outlook.com>
2022-11-22 12:39:49 +0100

    add

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,72 +1,27 @@
+# Input variable definitions
 variable "location" {
-  type    = string
-  default = "northeurope"
+  description = "The Azure Region in which all resources groups should be created."
+  type        = string
+  default = "eastus"
 }
-
 variable "resource_group_name" {
-  type    = string
-  default = "ibinderresourcegroup"
+  description = "The name of the resource group"
+  type        = string
+  default = "rgdev"
 }
-
 variable "storage_account_name" {
-  type    = string
-  default = "ibinderstorage"
+  description = "The name of the storage account"
+  type        = string
+  default = "abcde"
 }
-
-variable "storage_container_name" {
-  type    = string
-  default = "ibinderstoragecontainer"
-}
-
-variable "account_replication_type" {
-  type    = string
-  default = "LRS"
-}
-
-variable "account_tier" {
-  type    = string
+variable "storage_account_tier" {
+  description = "Storage Account Tier"
+  type        = string
   default = "Standard"
 }
 
-
-variable "app_service_name" {
-  type    = string
-  default = "ibinder"
-}
-
-variable "application_insights_name" {
-  type    = string
-  default = "ibinderappinsights"
-}
-
-
-variable "application_type" {
-  type    = string
-  default = "web"
-}
-
-variable "storage_account_access_key" {
-  type    = string
-  default = "https://ibinderkeyvaultstore.vault.azure.net/secrets/StorageAccountKey/fb39436bc0ac4e149c693e71fda977bd"
-}
-
-variable "app_service_plan_id" {
-  type    = string
-  default = ""
-}
-
-variable "function_app_name" {
-  type    = string
-  default = "ibinderfunctionapp"
-}
-
-
-variable "subscription_id" {
-  type    = string
-  default = "ae6cbacb-2eac-42cc-978e-516b8ef7628d"
-}
-
-variable "tenant_id" {
-  type    = string
-  default = "7bb1a8e5-59ee-489d-86f5-a50210ae3970"
+variable "virtual_network_name" {
+  description = "Virtual Network Name"
+  type        = string
+  default = "vnet"
 }

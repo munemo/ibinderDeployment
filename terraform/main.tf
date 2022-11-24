d28278ea7cb89ebf81f05c7f0271580bce48d013
munemo <john.chimbani@outlook.com>
2022-11-24 14:25:57 +0100

    new service principal

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,15 +5,12 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-module "azurerm_resource_group" {
-  source              = "./resource_group"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-}
-
 module "azurerm_service_plan" {
   source                = "./app_service_plan"
   resource_group_name   = var.resource_group_name
   app_service_plan_name = var.app_service_plan_name
   location              = var.location
+  depends_on = [
+    module.azurerm_resource_group
+  ]
 }
